8f2a815c76409ef33c66379c9928eff1074a1659
Leblanc Kévin <kevin.courriels@gmail.com>
2023-03-31 21:58:51 +0200

    Revert "Modification de la copie du côté violet en côté orange"
    This reverts commit 798c9687246c7dfd46ac8cba1ef4ba0a8f6995b6.

--- script.js
+++ script.js
@@ -23,6 +23,11 @@ const ElMap = document.getElementById("map");
 const ElDiscordUsername = document.getElementById("discord-username");
 const ElDiscordTooltip = document.getElementById("discord-tooltip");
 const ElPurplePicks = document.getElementById("purple-picks");
+const ElPurpleAttackers = document.getElementById("purple-attackers");
+const ElPurpleDefenders = document.getElementById("purple-defenders");
+const ElPurpleSupporters = document.getElementById("purple-supporters");
+const ElPurpleAllrounders = document.getElementById("purple-allrounders");
+const ElPurpleSpeedsters = document.getElementById("purple-speedsters");
 const ElOrangePicks = document.getElementById("orange-picks");
 const ElNeutralPicks = document.getElementById("neutral-picks");
 const ElBasePicks = document.getElementById("base-picks");
@@ -99,7 +104,7 @@ function onDrop(e) {
     pointY: 0,
     clone: dragData.clone
   };
-
+  console.log(1, dragData, marker)
   if (!dragData.dropped) {
     if (dragData.clone) {
       elem = elem.cloneNode(true);
@@ -160,16 +165,57 @@ function addZoomEvents() {
 }
 
 function copyPurpleToOrangePicks() {
-  const clonePurple = ElPurplePicks.cloneNode(true);
-  for(let i = 0; i < clonePurple.children.length; i++) {
-    clonePurple.children[i].classList.remove("purple-side");
-    clonePurple.children[i].classList.add("orange-side");
-    for (let j = 0; j < clonePurple.children[i].children.length; j++) {
-      clonePurple.children[i].children[j].classList.remove("purple-mon");
-      clonePurple.children[i].children[j].classList.add("orange-mon");
-    }
-    ElOrangePicks.appendChild(clonePurple.children[i]);
+  const cloneAtk = ElPurpleAttackers.cloneNode(true);
+  const cloneDfd = ElPurpleDefenders.cloneNode(true);
+  const cloneSup = ElPurpleSupporters.cloneNode(true);
+  const cloneArd = ElPurpleAllrounders.cloneNode(true);
+  const cloneSpd = ElPurpleSpeedsters.cloneNode(true);
+
+  cloneAtk.id = "orange-attackers";
+  cloneAtk.classList.remove("purple-side");
+  cloneAtk.classList.add("orange-side");
+  for (let i = 0; i < cloneAtk.children.length; i++) {
+    cloneAtk.children[i].classList.remove("purple-mon");
+    cloneAtk.children[i].classList.add("orange-mon");
   }
+
+  cloneDfd.id = "orange-defenders";
+  cloneDfd.classList.remove("purple-side");
+  cloneDfd.classList.add("orange-side");
+  for (let i = 0; i < cloneDfd.children.length; i++) {
+    cloneDfd.children[i].classList.remove("purple-mon");
+    cloneDfd.children[i].classList.add("orange-mon");
+  }
+
+  cloneSup.id = "orange-supporters";
+  cloneSup.classList.remove("purple-side");
+  cloneSup.classList.add("orange-side");
+  for (let i = 0; i < cloneSup.children.length; i++) {
+    cloneSup.children[i].classList.remove("purple-mon");
+    cloneSup.children[i].classList.add("orange-mon");
+  }
+
+  cloneArd.id = "orange-allrounders";
+  cloneArd.classList.remove("purple-side");
+  cloneArd.classList.add("orange-side");
+  for (let i = 0; i < cloneArd.children.length; i++) {
+    cloneArd.children[i].classList.remove("purple-mon");
+    cloneArd.children[i].classList.add("orange-mon");
+  }
+
+  cloneSpd.id = "orange-speedsters";
+  cloneSpd.classList.remove("purple-side");
+  cloneSpd.classList.add("orange-side");
+  for (let i = 0; i < cloneSpd.children.length; i++) {
+    cloneSpd.children[i].classList.remove("purple-mon");
+    cloneSpd.children[i].classList.add("orange-mon");
+  }
+  
+  ElOrangePicks.appendChild(cloneAtk);
+  ElOrangePicks.appendChild(cloneDfd);
+  ElOrangePicks.appendChild(cloneSup);
+  ElOrangePicks.appendChild(cloneArd);
+  ElOrangePicks.appendChild(cloneSpd);
 }
 
 function copyToClipboard(text) {
